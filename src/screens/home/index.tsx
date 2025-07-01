100ebede6343fbe12314770486f3dcfdea51b9fd
Lidobix <pipoflutepouet@gmail.com>
2025-07-01 15:18:11 +0200

    dev position bouton explore

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -43,7 +43,7 @@ const HomeScreen = () => {
 
         <div className="explore_button_wrapper">
           <div className="explore_button">
-            <span className="explore_text text4">EXPLORE</span>
+            <span className="explore_text text12">EXPLORE</span>
           </div>
         </div>
       </div>
